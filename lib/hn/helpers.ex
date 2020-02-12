1d6b595cf6c05bbdb418d4bf92671cf6fa1fb4b7
Josh Rotenberg <joshua.d.rotenberg@aexp.com>
2020-02-11 17:27:36 -0800

    pass a fn for fetch_all

--- lib/hn/helpers.ex
+++ lib/hn/helpers.ex
@@ -3,15 +3,15 @@ defmodule HN.Helpers do
 
   def fetch_stories(ids, limit \\ nil) do
     case ids do
-      {:ok, ids} -> fetch_all(ids, limit)
-      ids -> fetch_all(ids, limit)
+      {:ok, ids} -> fetch_all(ids, &HN.item!/1, limit)
+      ids -> fetch_all(ids, &HN.item!/1, limit)
     end
   end
 
-  defp fetch_all(ids, limit) do
+  defp fetch_all(ids, func, limit) do
     ids
     |> take_or_all(limit)
-    |> Enum.map(&Task.async(fn -> HN.item!(&1) end))
+    |> Enum.map(&Task.async(fn -> func.(&1) end))
     |> Enum.map(&Task.await/1)
   end
 
